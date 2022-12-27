860f8c6b2422fd99ef5b8f712a581a0141780bc7
cerberina <cerberina25@gmail.com>
2022-12-27 16:42:43 +0100

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference "User.count" do
       post users_path, params: { user: { name: "", email: "user@invalid", 
-                                  password: "foo", password_confirmation: "bar" } }
+                                        password: "foo", password_confirmation: "bar" } }
     end
     assert_template "users/new"
     assert_select "div#error_explanation"
@@ -13,5 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors input[name="user[email]"]'
     assert_select 'div.field_with_errors input[name="user[password]"]'
     assert_select 'div.field_with_errors input[name="user[password_confirmation]"]'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+    post users_path, params: { user: { name: "Example User", email: "user@example.com",
+                                        password: "password",password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty? 
   end
 end
